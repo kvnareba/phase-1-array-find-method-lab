33591ca279d8be91674cf2f3e94715589844d6f6
kvn <kvnareba@gmail.com>
2024-04-16 10:59:30 +0300

    com

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    
+  ]
+  console.log(superbowlWin(record));
+
+
+function superbowlWin(record){
+
+   const win = record.find(({result}) => result === "W");
+   if(win){
+    return win.year
+   }  
+   else {
+    return undefined
+   } 
+}
